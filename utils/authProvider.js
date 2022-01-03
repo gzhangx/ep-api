641c8ebc701bee1ded9ccf4b4d3c4904ecc1443a
Gang Zhang <gzhangx@hotmail.com>
2022-01-03 11:39:49 -0500

    typescript

--- utils/authProvider.js
+++ utils/authProvider.js
@@ -2,18 +2,32 @@ const crypto = require('crypto');
 const uuid = require('uuid')
 const secret = {
     seed: uuid.v1(),
+    pwdSeed: 'somethingUniqueDontChange',
 }
 
 function getUserKey(user) {
     return `${user.username}:${user.password}`;
 }
 
+///
+// user: username, password, provider, id, created, verified
+//
+//
+///
 function getUserHash(user) {
     return crypto
         .createHmac('sha256', secret.seed)
         .update(getUserKey(user))
         .digest('hex');
 }
+
+function getPwdHash(pwd) {
+    return crypto
+        .createHmac('sha256', secret.pwdSeed)
+        .update(pwd)
+        .digest('hex');
+}
+
 function signUser(user) {
     return {
         ...user,
@@ -29,4 +43,5 @@ function verifyUser(user) {
 module.exports = {
     signUser,
     verifyUser,
+    getPwdHash,
 }

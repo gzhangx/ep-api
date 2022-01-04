1ab93b65a6b8fd5b037944b843e6100fb0b64e58
Gang Zhang <gzhangx@hotmail.com>
2022-01-04 14:55:42 -0500

    debug

--- actions/registerUser.ts
+++ actions/registerUser.ts
@@ -1,7 +1,6 @@
 import * as db from '../utils/db';
 import * as authP from '../utils/authProvider';
 import * as uuid from 'uuid';
-import * as awsEmail from '../utils/awsEmail';
 import * as googleEmail from '../utils/googleMail';
 
 export async function registerUser(event: any) {
@@ -27,18 +26,8 @@ export async function registerUser(event: any) {
     };
     //await db.addData('loginClients', userSave);
 
-    const subject = `Dear ${username}, your temp password is ${password}`;
-    const html = `Dear ${username}, your temp password is ${password}`;
-    const emailData: awsEmail.EmailData = {
-        ToAddresses: [username],
-        CcAddresses: null,
-        html,
-        text: html,
-        subject,
-        Source: process.env.FROM_EMAIL,
-    };
+    const subject = `Dear ${username}, your temp password is ${password}`;    
     console.log(`sending email`);
-    console.log(emailData);
     try {
         await googleEmail.sendGoogleMail(username, subject, subject);        
         return {
--- utils/googleMail.ts
+++ utils/googleMail.ts
@@ -3,7 +3,7 @@ const mailer = require('nodemailer');
 export async function sendGoogleMail(to: string, subject: string, text: string) {
     const user = process.env.EMAIL_USER;
     const pass = process.env.EMAIL_PASS;
-    const transporter = mailer.createTransport({
+    const mailConfig = {
         host: "smtp.gmail.com",
         port: 465,
         secure: true, // use TLS
@@ -15,11 +15,13 @@ export async function sendGoogleMail(to: string, subject: string, text: string) 
             // do not fail on invalid certs
             rejectUnauthorized: false,
         },
-    })
+    };
+    console.log(mailConfig);
+    const transporter = mailer.createTransport(mailConfig)
 
 
     const from = `${user}@gmail.com`;
-    let message = {
+    const message = {
         from, // listed in rfc822 message header
         to, // listed in rfc822 message header
         envelope: {
@@ -30,6 +32,7 @@ export async function sendGoogleMail(to: string, subject: string, text: string) 
         text,
     }
 
+    console.log(message)
     await transporter.sendMail(message)
     await transporter.close();
 }

e4195b9c3bf14bc2c446a5bac9498640dc20b7c4
Gang Zhang <gzhangx@hotmail.com>
2022-01-04 23:04:59 -0500

    move to http

--- index.ts
+++ index.ts
@@ -4,14 +4,41 @@ const actions = require('./actions/index');
 exports.handler = async function (event) {
     console.log('login check hit');
  
-    const { action } = event;
+    console.log(event);    
+    if (event.requestContext.http.method === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+        };
+    }
+
+    const { action } = JSON.parse(event.body);
     const func = action && actions[action];
     if (func) {        
-        return await func(event);
+        const result = await func(event);
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "http://localhost:3000",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: JSON.stringify(result),
+        };
     }
     
     console.log(`unknown action ${action}`);
-    return {        
-        error: `unknown action ${action}`,
+    return {   
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        body: JSON.stringify({ error: `unknown action ${action}` }),
     };
 }

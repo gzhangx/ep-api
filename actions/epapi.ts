53184f63180bf9ffea1d459b28bf77ccd544f513
Gang Zhang <gzhangx@hotmail.com>
2022-01-14 12:20:48 -0500

    fix url

--- actions/epapi.ts
+++ actions/epapi.ts
@@ -1,7 +1,7 @@
 import * as request from 'superagent';
 
 export async function epApiQuery(event) {
-    const baseUrl = 'https://equipmentgram.com/wp-json/gf/v2';
+    const baseUrl = 'https://equipmentgram.com/wp-json/gf/v2/forms';
     const { formId, pageSize, currentPage } = event;
     const { EQAPI_GFKEY, EQAPI_GFSEC } = process.env;
     if (!EQAPI_GFKEY || !EQAPI_GFSEC) {
@@ -14,9 +14,9 @@ export async function epApiQuery(event) {
     const sec = 'Basic ' + Buffer.from(`${EQAPI_GFKEY}:${EQAPI_GFSEC}`).toString('base64');
     let url = '';
     if (!formId) {
-        url = `${baseUrl}/forms`;
+        url = baseUrl;
     } else {
-        url = `${baseUrl}/${parseInt(formId)}/entries?sorting[key]=id&sorting[direction]=DESC&sorting[is_numeric]=true&paging[page_size]=${parseInt(pageSize || 20)}&paging[current_page]=${parseInt(currentPage || 0)}`;                
+        url = `${baseUrl}/${parseInt(formId)}/entries?sorting[key]=id&sorting[direction]=DESC&sorting[is_numeric]=true&paging[page_size]=${parseInt(pageSize || 20)}&paging[current_page]=${parseInt(currentPage || 1)}`;
     }
     console.log(url);
     return request.get(url).set('Authorization', sec).send().then(r => r.body);

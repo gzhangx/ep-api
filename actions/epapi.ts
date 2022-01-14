c64ff5309f088ee916431696fb055884fb35e0cc
Gang Zhang <gzhangx@hotmail.com>
2022-01-14 12:11:16 -0500

    added eqapi

---
+++ actions/epapi.ts
@@ -0,0 +1,23 @@
+import * as request from 'superagent';
+
+export async function epApiQuery(event) {
+    const baseUrl = 'https://equipmentgram.com/wp-json/gf/v2';
+    const { formId, pageSize, currentPage } = event;
+    const { EQAPI_GFKEY, EQAPI_GFSEC } = process.env;
+    if (!EQAPI_GFKEY || !EQAPI_GFSEC) {
+        const error = (`EQAPI_GFKEY and EQAPI_GFSEC must be set`);
+        console.log(error);
+        return {
+            error,
+        }
+    }
+    const sec = 'Basic ' + Buffer.from(`${EQAPI_GFKEY}:${EQAPI_GFSEC}`).toString('base64');
+    let url = '';
+    if (!formId) {
+        url = `${baseUrl}/forms`;
+    } else {
+        url = `${baseUrl}/${parseInt(formId)}/entries?sorting[key]=id&sorting[direction]=DESC&sorting[is_numeric]=true&paging[page_size]=${parseInt(pageSize || 20)}&paging[current_page]=${parseInt(currentPage || 0)}`;                
+    }
+    console.log(url);
+    return request.get(url).set('Authorization', sec).send().then(r => r.body);
+}
